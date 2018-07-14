f07f03407c9138e143de321256e3ced5aea76bc6
Luis Benitez <luisbenitez02@gmail.com>
2018-07-13 22:22:44 -0500

    varaibles con var
    Ejemplos y explicacion sobre el uso de var en JS.

---
+++ js/udemy-js-guia-2018/1-Fundamentos/01-variables/script.js
@@ -0,0 +1,18 @@
+/*
+Hay dos formas de agregar variables
+
+var y let
+*/
+
+/* Varaibles Globales con var */
+var name = "Viviana";
+var name = "Luis";
+
+var vacia;//varaibles vacias
+console.log(name);
+console.log(vacia);
+
+var uno = 1, dos=2;//declaradas juntas
+console.log(uno + dos);
+
+
