9d746e5d7c379244bf2c100c11468ef40afd19af
Luis Benitez <luisbenitez02@gmail.com>
2018-07-15 18:25:23 -0500

    Strings
    Aprendiendo algunas funciones con strings

--- js/udemy-js-guia-2018/1-Fundamentos/01-variables/script.js
+++ js/udemy-js-guia-2018/1-Fundamentos/01-variables/script.js
@@ -15,4 +15,21 @@ console.log(vacia);
 var uno = 1, dos=2;//declaradas juntas
 console.log(uno + dos);
 
+/**
+ * Variables con let: locales
+ */
+let mascota ="paquita";
+console.log(mascota);
 
+//No puedes renombrar una variable con let
+//saldra error
+let producto="Jitomate";
+//let producto="Berenjena";//genera error
+//puedes modificar pero no redeclarar
+
+/**
+ * Variables con const
+ */
+const fijo = 28;
+//fijo=15;//NO permite redeclarar y siempre se deben declarar al principio
+console.log("Hola" + fijo);
---
+++ js/udemy-js-guia-2018/1-Fundamentos/02-strings/script.js
@@ -0,0 +1,10 @@
+let lenguaje = "Javascript";
+console.log(lenguaje.length);
+console.log(lenguaje.toUpperCase());
+console.log(lenguaje.toLowerCase());
+
+let texto = "Es un lenguaje de programacion presente en gran cantidad de dispositivos";
+
+console.log(texto.indexOf('gran'));//paso palabra que busco
+
+
