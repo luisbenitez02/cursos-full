3ed02d4f935dfaadade299020c3636d071605d6f
Luis Benitez <luisbenitez02@gmail.com>
2018-07-15 20:45:20 -0500

    Numeros (Math)
    Funciones de la libreria Math

---
+++ js/udemy-js-guia-2018/1-Fundamentos/03-numeros/script.js
@@ -0,0 +1,13 @@
+console.log(Math.PI);
+print('round',Math.round(2.56));
+print('ceil',Math.ceil(4.6));
+print('floor', Math.floor(4.6))
+print('pow', Math.pow(4,2));
+print('min',Math.min(3,5,4,2,9));
+print('random',Math.random());
+print('Viviana', Math.sqrt(16));//raiz de 16
+
+function print(d,n) {
+     console.log(d+": ",n);
+     
+}
