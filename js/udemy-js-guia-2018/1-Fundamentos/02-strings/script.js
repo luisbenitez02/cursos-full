4c17f5e262dbeaefae98a70bedddf394084c43ad
Luis Benitez <luisbenitez02@gmail.com>
2018-07-15 20:08:11 -0500

    Metodos string 2
    Nuevos metodos para string como repalce, repeat, includes

--- js/udemy-js-guia-2018/1-Fundamentos/02-strings/script.js
+++ js/udemy-js-guia-2018/1-Fundamentos/02-strings/script.js
@@ -3,8 +3,21 @@ console.log(lenguaje.length);
 console.log(lenguaje.toUpperCase());
 console.log(lenguaje.toLowerCase());
 
-let texto = "Es un lenguaje de programacion presente en gran cantidad de dispositivos";
+let t = "Es un lenguaje de programacion presente en gran cantidad de dispositivos";
 
-console.log(texto.indexOf('gran'));//paso palabra que busco
+console.log(t.indexOf('gran'));//paso palabra que busco
+console.log(t.substring(0,14));
+console.log(t.slice(-20));//de atras hacia adelante
+console.log(t.split(' '));//divide lo ingresado y lo porciona
+console.log(t.replace('programacion','de super programacion'));
+console.log(t.includes('dispositivos'));
+
+console.log(lenguaje.repeat(7));
+
+
+
+
+
+
 
 
