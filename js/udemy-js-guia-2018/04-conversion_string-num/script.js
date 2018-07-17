6255e3d85a10ab962bdd9ec3e30e8f699f6ba59c
Luis Benitez <luisbenitez02@gmail.com>
2018-07-16 20:42:53 -0500

    Convertir string a numero y viceversa
    Funciones para convertir datos

---
+++ js/udemy-js-guia-2018/04-conversion_string-num/script.js
@@ -0,0 +1,12 @@
+let  numero = 50,
+     numStr = "60",
+     numerito= 45.254785;
+
+console.log(Number(numStr)+numero);
+console.log(parseInt(numStr));
+console.log(numerito.toFixed(3));
+console.log(String(numero));
+console.log(numerito.toString());
+
+
+
