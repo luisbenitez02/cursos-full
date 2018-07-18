d5b9d14075f0d610edbad5fd991b7825bb2e203a
Luis Benitez <luisbenitez02@gmail.com>
2018-07-18 00:14:53 -0500

    Objetos
    Aprende a crear objetos en JS con funciones y otros objetos adentro

--- js/udemy-js-guia-2018/1-Fundamentos/07-objetosYarreglos/script.js
+++ js/udemy-js-guia-2018/1-Fundamentos/07-objetosYarreglos/script.js
@@ -1,57 +1,35 @@
-const numeros = [1,2,3,4,5,6,7,8,9,0];
+//creando objetos
+const persona = {
+     nombre: "Elbert",
+     apellido: "Gomez Torba",
+     edad: 23,
+     profesion: 'Ingenierio de sistemas',
+     email: 'elbertgomeztorba@gmail.com',
+     musica: ['salsa','bachata','Merengue'],
+     ubicacion: {
+          pais:'Colombia',
+          ciudad: 'Palmira'
+     },
+     hablar: function () {
+         return "Hola papus" + this.apellido;   
+     }
+}
 
-console.log(numeros);
+print('objeto',persona);
+print('objeto.dato',persona.nombre);
+//imprimiendo elemento dentro de otro objeto
+print('objeto dentro de otro', persona.ubicacion.ciudad);
+//ejecutando una funcion
+print('Funcion dentro de objeto',persona.hablar());
 
-//metodo alternativo de arreglos
-const names = new Array('Luis','viviana','Marta','Sofia','Calocha');
-
-console.log(names);
-console.log(Array.isArray(names));
-
-//funciones con arreglos
-console.log(names.indexOf('Luis'));
-
-names.push('Juanita');
-console.log(names);
-
-names.unshift('Electra');
-console.log(names);
-
-names.pop();//remueve ultimo elemento
-console.log(names);
-
-names.shift();//remueve el primero
-console.log(names);
-
-names.splice(2,2);//a partir de la posicion 2 quita 2 elementos
-console.log(names);
-
-names.reverse();//cambia orden
-
-console.log(names.concat(numeros));
-
-const frutas = ['fresa','naranja','manzana','platano'];
-
-//ordenar un arreglo con letras
-frutas.sort();
-console.log(frutas);
-
-const numeritos = [25,4,1,6,9,8,4,55,11,21,100,95];
-
-numeritos.sort(function(x,y){
-     return x-y;
-});
-
-console.log(numeritos);
-
-//de menor a mayor solo invertimos la resta
-numeritos.sort(function(x,y){
-     return y-x;
-});
-
-console.log(numeritos);
-
-
+print('Acceso a los mismos elementos', persona.hablar() );//desde el objeto
+     
+/**
+ * Funcion para imprimir
+ */
+function print(name,data){
+     console.log(name+' :',data);
+}
 
 
 
