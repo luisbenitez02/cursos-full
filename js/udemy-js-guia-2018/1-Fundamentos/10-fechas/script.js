5e1f178709bcfb4be4f6911e2df921ee5880ac2a
Luis Benitez <luisbenitez02@gmail.com>
2018-07-21 22:13:29 -0500

    Fechas
    Manejo de fechas a traves del objeto Date

---
+++ js/udemy-js-guia-2018/1-Fundamentos/10-fechas/script.js
@@ -0,0 +1,23 @@
+const diaHoy = new Date();
+
+console.log(diaHoy);
+
+//fecha especifica
+//se debe pasar en orden: anio-mes-dia
+let navidad2017 = new Date('2017-12-25');
+console.log(navidad2017);
+
+//mes actual
+console.log(diaHoy.getMonth());
+//dia
+console.log(diaHoy.getDate());
+//anio
+console.log(diaHoy.getFullYear());
+//minutos
+console.log(diaHoy.getMinutes());
+//hora
+console.log(diaHoy.getHours());
+
+
+
+
